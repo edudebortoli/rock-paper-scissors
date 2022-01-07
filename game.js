0565a897227e1041a2321bc9e1a678f6de837c4a
Eduardo Debortoli Piccoli <edudebortoli@icloud.com>
2022-01-07 15:26:45 -0300

    EventListener and scrap functions

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
-function game (){
+const roundEvent = document.querySelector("#round-event");
 
-    function roundStart()
-        
+const rps = document.querySelectorAll(".rps");
 
+function selectPcHand(){
+    let randomNumber = Math.random();
 
+    for(let x = 0; x < 12; x++){
+
+    }
+    
+    if (randomNumber < 0.3333){
+        return("rock")
+    } else if (randomNumber > 0.666){
+        return("scissor")
+    } else {
+        return("paper")
+    }
 }
+
+function roundStart(playerHand){
+    console.log(`${playerHand} x ${selectPcHand()}`)
+}
+
+
+
+function selectHand(a){
+    roundStart(a.target.id);
+}
+
+console.log(rps);
+
+window.addEventListener("click", selectHand);
